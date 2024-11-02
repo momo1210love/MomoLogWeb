40dee1b5a6f222bc483f4b3d083d2cf4cd0152b6
山城李香 <>
2024-11-02 13:45:50 +0900

    First commit

---
+++ src/main/java/com/example/MomoLogWeb/MomoLogWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.MomoLogWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomoLogWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomoLogWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MomoLogWeb/MomoLogWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MomoLogWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoLogWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
